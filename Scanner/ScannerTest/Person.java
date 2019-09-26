7bd5d341a995149b66cfb62f3c3cbce5a6f75e23
797529 <54371358+797529@users.noreply.github.com>
2019-09-26 10:10:38 -0700

    working mode

--- Scanner/ScannerTest/Person.java
+++ Scanner/ScannerTest/Person.java
@@ -8,9 +8,31 @@
 public class Person
 {
     public Person(String name){
-        int divider = name.indexOf(" ");
-        int firstdiv = name.indexOf(",");
-        System.out.println("First Name : " + name.substring(0,divider));
-        System.out.println("Last Name : " + name.substring(divider));
+        int midname = name.indexOf(" ");
+        int lastname = name.lastIndexOf(" ");
+        int lastfirst = name.indexOf(",");
+        if (lastfirst == -1){
+            if(midname != lastname){
+                System.out.println("First Name : " + name.substring(0,midname));
+                System.out.println("Middle Name : " + name.substring(midname,lastname));
+                System.out.println("Last Name : " + name.substring(lastname));
+            }
+            else{
+                System.out.println("First Name : " + name.substring(0,lastname));
+                System.out.println("Last Name : " + name.substring(lastname));
+         }
+
+        }
+        else if(lastfirst != -1){
+            if(midname != -1){
+                System.out.println("First Name : " + name.substring(lastfirst,lastname));
+                System.out.println("Middle Name : " + name.substring(lastname));
+                System.out.println("Last Name : " + name.substring(0,lastfirst));
+            }
+            else{
+            System.out.println("First Name : " + name.substring(lastfirst));
+            System.out.println("Last Name : " + name.substring(0,lastfirst));
+           }
+        }
     }
 }
