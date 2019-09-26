95632fb20b5b131368ddd080197f517d435e0e99
797529 <54371358+797529@users.noreply.github.com>
2019-09-26 10:30:14 -0700

    finished lab 926

--- Scanner/ScannerTest/NameParser.java
+++ Scanner/ScannerTest/NameParser.java
@@ -9,14 +9,23 @@ import java.util.Scanner;
 public class NameParser{
     public static void main(){
         Scanner kb = new Scanner(System.in);
+        String[] namearr = new String[10];
+        int count = 0;
         String inputStr = "";
         String lastName = "";
         //if(str1 == str2)
         while(!lastName.equals("quit")){
             System.out.println("Enter a name or type \"quit\" to quit");
             lastName = kb.nextLine();
+            if(lastName.equals("print names")){
+                for(int i = 0; i < namearr.length; i++){
+                System.out.println(namearr[i]);
+              }
+            }
             Person fullnm = new Person(lastName);
             System.out.println(fullnm);
+            namearr[count] = lastName;
+            count+=1;
         }
     }
 }
