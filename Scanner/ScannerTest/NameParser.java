8d10a42f21234d66d834eefdd18bc5209dabad06
797529 <54371358+797529@users.noreply.github.com>
2019-11-06 10:18:05 -0800

    bruh

--- Scanner/ScannerTest/NameParser.java
+++ Scanner/ScannerTest/NameParser.java
@@ -12,27 +12,33 @@ public class NameParser{
         ArrayList<Person> cls = new ArrayList<Person>(10);
         String[] stuIDs = new String[10];
         int count = 0;
+        String stuGPA = "";
+        String userinput = "";
         String studentID = "";
         String inputStr = "";
         String lastName = "";
         //if(str1 == str2)
         while(!lastName.equals("quit")){
-            System.out.println("Enter a name or type \"quit\" to quit");
-            lastName = kb.nextLine();
-            System.out.println("Type this student's ID:");
-            studentID = kb.nextLine();
-            stuIDs[count] = studentID;
-            if(lastName.equals("print names")){
-                for(int i = 0; i < cls.size(); i++){
-                    if(cls.get(i) != null){
-                System.out.println(cls.get(i).returnFullName() + " " + stuIDs[i]);
-              }
-             }
-             continue;
+            System.out.println("Select an option below: \n1) Add Student \n2) Delete student\n3) Edit Student\n4) Reset List\n5) Print List\n6) Print Student\n7) Sort Student");
+            userinput = kb.nextLine();
+            if(userinput.equals("1")){
+                System.out.println("Enter a name or type \"quit\" to quit");
+                lastName = kb.nextLine();
+                System.out.println("Type this student's ID:");
+                studentID = kb.nextLine();
+                stuIDs[count] = studentID;
+                Person fullnm = new Person(lastName);
+                cls.add(fullnm);
+                count+=1;
             }
-            Person fullnm = new Person(lastName);
-            cls.add(fullnm);
-            count+=1;
+            if(userinput.equals("5")){
+                    for(int i = 0; i < cls.size(); i++){
+                        if(cls.get(i) != null){
+                    System.out.println(cls.get(i).returnFullName() + " " + stuIDs[i]);
+                  }
+                 }
+                 continue;
+            }
         }
     }
 }
