a77eb833f63b397086859b31cfdd87d9e0cd556a
797529 <54371358+797529@users.noreply.github.com>
2019-12-02 10:26:02 -0800

    extra extra

--- PictureLabProject/pixLab/classes/Picture.java
+++ PictureLabProject/pixLab/classes/Picture.java
@@ -17,218 +17,452 @@ import java.util.List; // resolves problem with java.awt.List and java.util.List
  */
 public class Picture extends SimplePicture 
 {
-  ///////////////////// constructors //////////////////////////////////
-  
-  /**
-   * Constructor that takes no arguments 
-   */
-  public Picture ()
-  {
-    /* not needed but use it to show students the implicit call to super()
-     * child constructors always call a parent constructor 
-     */
-    super();  
-  }
-  
-  /**
-   * Constructor that takes a file name and creates the picture 
-   * @param fileName the name of the file to create the picture from
-   */
-  public Picture(String fileName)
-  {
-    // let the parent class handle this fileName
-    super(fileName);
-  }
-  
-  /**
-   * Constructor that takes the width and height
-   * @param height the height of the desired picture
-   * @param width the width of the desired picture
-   */
-  public Picture(int height, int width)
-  {
-    // let the parent class handle this width and height
-    super(width,height);
-  }
-  
-  /**
-   * Constructor that takes a picture and creates a 
-   * copy of that picture
-   * @param copyPicture the picture to copy
-   */
-  public Picture(Picture copyPicture)
-  {
-    // let the parent class do the copy
-    super(copyPicture);
-  }
-  
-  /**
-   * Constructor that takes a buffered image
-   * @param image the buffered image to use
-   */
-  public Picture(BufferedImage image)
-  {
-    super(image);
-  }
-  
-  ////////////////////// methods ///////////////////////////////////////
-  
-  /**
-   * Method to return a string with information about this picture.
-   * @return a string with information about the picture such as fileName,
-   * height and width.
-   */
-  public String toString()
-  {
-    String output = "Picture, filename " + getFileName() + 
-      " height " + getHeight() 
-      + " width " + getWidth();
-    return output;
-    
-  }
-  
-  /** Method to set the blue to 0 */
-  public void zeroBlue()
-  {
-    Pixel[][] pixels = this.getPixels2D();
-    for (Pixel[] rowArray : pixels)
-    {
-      for (Pixel pixelObj : rowArray)
-      {
-        pixelObj.setBlue(0);
-      }
-    }
-  }
-  
-  /** Method that mirrors the picture around a 
-    * vertical mirror in the center of the picture
-    * from left to right */
-  public void mirrorVertical()
-  {
-    Pixel[][] pixels = this.getPixels2D();
-    Pixel leftPixel = null;
-    Pixel rightPixel = null;
-    int width = pixels[0].length;
-    for (int row = 0; row < pixels.length; row++)
-    {
-      for (int col = 0; col < width / 2; col++)
-      {
-        leftPixel = pixels[row][col];
-        rightPixel = pixels[row][width - 1 - col];
-        rightPixel.setColor(leftPixel.getColor());
-      }
-    } 
-  }
-  
-  /** Mirror just part of a picture of a temple */
-  public void mirrorTemple()
-  {
-    int mirrorPoint = 276;
-    Pixel leftPixel = null;
-    Pixel rightPixel = null;
-    int count = 0;
-    Pixel[][] pixels = this.getPixels2D();
-    
-    // loop through the rows
-    for (int row = 27; row < 97; row++)
-    {
-      // loop from 13 to just before the mirror point
-      for (int col = 13; col < mirrorPoint; col++)
-      {
-        
-        leftPixel = pixels[row][col];      
-        rightPixel = pixels[row]                       
-                         [mirrorPoint - col + mirrorPoint];
-        rightPixel.setColor(leftPixel.getColor());
-      }
-    }
-  }
-  
-  /** copy from the passed fromPic to the
-    * specified startRow and startCol in the
-    * current picture
-    * @param fromPic the picture to copy from
-    * @param startRow the start row to copy to
-    * @param startCol the start col to copy to
-    */
-  public void copy(Picture fromPic, 
-                 int startRow, int startCol)
-  {
-    Pixel fromPixel = null;
-    Pixel toPixel = null;
-    Pixel[][] toPixels = this.getPixels2D();
-    Pixel[][] fromPixels = fromPic.getPixels2D();
-    for (int fromRow = 0, toRow = startRow; 
-         fromRow < fromPixels.length &&
-         toRow < toPixels.length; 
-         fromRow++, toRow++)
-    {
-      for (int fromCol = 0, toCol = startCol; 
-           fromCol < fromPixels[0].length &&
-           toCol < toPixels[0].length;  
-           fromCol++, toCol++)
-      {
-        fromPixel = fromPixels[fromRow][fromCol];
-        toPixel = toPixels[toRow][toCol];
-        toPixel.setColor(fromPixel.getColor());
-      }
-    }   
-  }
+    ///////////////////// constructors //////////////////////////////////
 
-  /** Method to create a collage of several pictures */
-  public void createCollage()
-  {
-    Picture flower1 = new Picture("flower1.jpg");
-    Picture flower2 = new Picture("flower2.jpg");
-    this.copy(flower1,0,0);
-    this.copy(flower2,100,0);
-    this.copy(flower1,200,0);
-    Picture flowerNoBlue = new Picture(flower2);
-    flowerNoBlue.zeroBlue();
-    this.copy(flowerNoBlue,300,0);
-    this.copy(flower1,400,0);
-    this.copy(flower2,500,0);
-    this.mirrorVertical();
-    this.write("collage.jpg");
-  }
-  
-  
-  /** Method to show large changes in color 
-    * @param edgeDist the distance for finding edges
-    */
-  public void edgeDetection(int edgeDist)
-  {
-    Pixel leftPixel = null;
-    Pixel rightPixel = null;
-    Pixel[][] pixels = this.getPixels2D();
-    Color rightColor = null;
-    for (int row = 0; row < pixels.length; row++)
+    /**
+     * Constructor that takes no arguments 
+     */
+    public Picture ()
     {
-      for (int col = 0; 
-           col < pixels[0].length-1; col++)
-      {
-        leftPixel = pixels[row][col];
-        rightPixel = pixels[row][col+1];
-        rightColor = rightPixel.getColor();
-        if (leftPixel.colorDistance(rightColor) > 
-            edgeDist)
-          leftPixel.setColor(Color.BLACK);
-        else
-          leftPixel.setColor(Color.WHITE);
-      }
+        /* not needed but use it to show students the implicit call to super()
+         * child constructors always call a parent constructor 
+         */
+        super();  
     }
-  }
-  
-  
-  /* Main method for testing - each class in Java can have a main 
-   * method 
-   */
-  public static void main(String[] args) 
-  {
-    Picture beach = new Picture("beach.jpg");
-    beach.explore();
-    beach.zeroBlue();
-    beach.explore();
-  }
-  
+
+    /**
+     * Constructor that takes a file name and creates the picture 
+     * @param fileName the name of the file to create the picture from
+     */
+    public Picture(String fileName)
+    {
+        // let the parent class handle this fileName
+        super(fileName);
+    }
+
+    /**
+     * Constructor that takes the width and height
+     * @param height the height of the desired picture
+     * @param width the width of the desired picture
+     */
+    public Picture(int height, int width)
+    {
+        // let the parent class handle this width and height
+        super(width,height);
+    }
+
+    /**
+     * Constructor that takes a picture and creates a 
+     * copy of that picture
+     * @param copyPicture the picture to copy
+     */
+    public Picture(Picture copyPicture)
+    {
+        // let the parent class do the copy
+        super(copyPicture);
+    }
+
+    /**
+     * Constructor that takes a buffered image
+     * @param image the buffered image to use
+     */
+    public Picture(BufferedImage image)
+    {
+        super(image);
+    }
+
+    ////////////////////// methods ///////////////////////////////////////
+
+    /**
+     * Method to return a string with information about this picture.
+     * @return a string with information about the picture such as fileName,
+     * height and width.
+     */
+    public String toString()
+    {
+        String output = "Picture, filename " + getFileName() + 
+            " height " + getHeight() 
+            + " width " + getWidth();
+        return output;
+
+    }
+
+    /** Method to set the blue to 0 */
+    public void zeroBlue()
+    {
+        Pixel[][] pixels = this.getPixels2D();
+        for (Pixel[] rowArray : pixels)
+        {
+            for (Pixel pixelObj : rowArray)
+            {
+                pixelObj.setBlue(0);
+            }
+        }
+    }
+
+    public void voidify()
+    {
+        Pixel[][] pixels = this.getPixels2D();
+        for (Pixel[] rowArray : pixels)
+        {
+            for (Pixel pixelObj : rowArray)
+            {
+                if(pixelObj.getRed() >= 127){
+                    pixelObj.setRed(255);
+                }
+                else if(pixelObj.getRed() < 127){
+                    pixelObj.setRed(0);
+                }
+                if(pixelObj.getBlue() >= 127){
+                    pixelObj.setBlue(255);
+                }
+                else if(pixelObj.getBlue() < 127){
+                    pixelObj.setBlue(0);
+                }
+                if(pixelObj.getGreen() >= 127){
+                    pixelObj.setGreen(255);
+                }
+                else if(pixelObj.getGreen() < 127){
+                    pixelObj.setGreen(0);
+                }
+            }
+        }
+    }
+
+    public void antivoidify()
+    {
+        Pixel[][] pixels = this.getPixels2D();
+        for (Pixel[] rowArray : pixels)
+        {
+            for (Pixel pixelObj : rowArray)
+            {
+                if(pixelObj.getRed() <= 127){
+                    pixelObj.setRed(255);
+                }
+                else if(pixelObj.getRed() > 127){
+                    pixelObj.setRed(0);
+                }
+                if(pixelObj.getBlue() <= 127){
+                    pixelObj.setBlue(255);
+                }
+                else if(pixelObj.getBlue() > 127){
+                    pixelObj.setBlue(0);
+                }
+                if(pixelObj.getGreen() <= 127){
+                    pixelObj.setGreen(255);
+                }
+                else if(pixelObj.getGreen() > 127){
+                    pixelObj.setGreen(0);
+                }
+            }
+        }
+    }
+
+    public void negativeImg(){
+        Pixel[][] pixels = this.getPixels2D();
+        for (Pixel[] rowArray : pixels)
+        {
+            for (Pixel pixelObj : rowArray)
+            {
+                pixelObj.setRed(255-pixelObj.getRed());
+                pixelObj.setGreen(255-pixelObj.getGreen());
+                pixelObj.setBlue(255-pixelObj.getBlue());
+            }
+        }
+    }
+
+    public void redmodePic()
+    {
+        Pixel[][] pixels = this.getPixels2D();
+        for (Pixel[] rowArray : pixels)
+        {
+            for (Pixel pixelObj : rowArray)
+            {
+                pixelObj.setRed(255);
+            }
+        }
+    }
+
+    public void greenPic()
+    {
+        Pixel[][] pixels = this.getPixels2D();
+        for (Pixel[] rowArray : pixels)
+        {
+            for (Pixel pixelObj : rowArray)
+            {
+                pixelObj.setGreen(255);
+            }
+        }
+    }
+
+    public void flippyVoid()
+    {
+        int counter = 0;
+        Pixel[][] pixels = this.getPixels2D();
+        for (Pixel[] rowArray : pixels)
+        {
+            for (Pixel pixelObj : rowArray)
+            {
+                counter++;
+                if(counter == 2){
+                    counter = 0;
+                    if(pixelObj.getRed() >= 127){
+                        pixelObj.setRed(255);
+                    }
+                    else if(pixelObj.getRed() < 127){
+                        pixelObj.setRed(0);
+                    }
+                    if(pixelObj.getBlue() >= 127){
+                        pixelObj.setBlue(255);
+                    }
+                    else if(pixelObj.getBlue() < 127){
+                        pixelObj.setBlue(0);
+                    }
+                    if(pixelObj.getGreen() >= 127){
+                        pixelObj.setGreen(255);
+                    }
+                    else if(pixelObj.getGreen() < 127){
+                        pixelObj.setGreen(0);
+                    }
+                }
+            }
+        }
+    }
+
+    public void flippyBlack()
+    {
+        int counter = 0;
+        int numbruh = 0;
+        int numbrah = 2;
+        int flip = 1;
+        Pixel[][] pixels = this.getPixels2D();
+        for (Pixel[] rowArray : pixels)
+        {
+            for (Pixel pixelObj : rowArray)
+            {
+                counter++;
+                if(counter == numbrah){
+                    counter = numbruh;
+                    pixelObj.setRed(255-pixelObj.getRed());
+                    pixelObj.setBlue(255-pixelObj.getBlue());
+                    pixelObj.setGreen(255-pixelObj.getGreen());
+                }
+            }
+            if(flip == 1){
+                numbruh = -1;
+                numbrah = 1;
+                flip = 0;
+                continue;
+            }
+            if(flip == 0){
+                numbruh = 0;
+                numbrah = 2;
+                flip = 1;
+                continue;
+            }
+        }
+    }
+
+    public void floppyBlack()
+    {
+        int counter = 0;
+        int numbruh = 0;
+        int numbrah = 2;
+        int flip = 1;
+        Pixel[][] pixels = this.getPixels2D();
+        for (Pixel[] rowArray : pixels)
+        {
+            for (Pixel pixelObj : rowArray)
+            {
+                counter++;
+                if(counter == 1){
+                    counter = -1;
+                    pixelObj.setBlue(0);
+                }
+            }
+        }
+    }
+
+    public void bluePic()
+    {
+        Pixel[][] pixels = this.getPixels2D();
+        for (Pixel[] rowArray : pixels)
+        {
+            for (Pixel pixelObj : rowArray)
+            {
+                pixelObj.setBlue(255);
+            }
+        }
+    }
+
+    public void zeroRed()
+    {
+        Pixel[][] pixels = this.getPixels2D();
+        for (Pixel[] rowArray : pixels)
+        {
+            for (Pixel pixelObj : rowArray)
+            {
+                pixelObj.setRed(0);
+            }
+        }
+    }
+
+    public void makeGrey()
+    {
+        int bruh=0;
+        Pixel[][] pixels = this.getPixels2D();
+        for (Pixel[] rowArray : pixels)
+        {
+            for (Pixel pixelObj : rowArray)
+            {
+                bruh = bruh + pixelObj.getGreen() + pixelObj.getRed() + pixelObj.getBlue();
+                bruh = bruh / 3;
+                pixelObj.setGreen(bruh);
+                pixelObj.setBlue(bruh);
+                pixelObj.setRed(bruh);
+            }
+        }
+    }
+
+    public void zeroGreen()
+    {
+        Pixel[][] pixels = this.getPixels2D();
+        for (Pixel[] rowArray : pixels)
+        {
+            for (Pixel pixelObj : rowArray)
+            {
+                pixelObj.setGreen(0);
+            }
+        }
+    }
+
+    /** Method that mirrors the picture around a 
+     * vertical mirror in the center of the picture
+     * from left to right */
+    public void mirrorVertical()
+    {
+        Pixel[][] pixels = this.getPixels2D();
+        Pixel leftPixel = null;
+        Pixel rightPixel = null;
+        int width = pixels[0].length;
+        for (int row = 0; row < pixels.length; row++)
+        {
+            for (int col = 0; col < width / 2; col++)
+            {
+                leftPixel = pixels[row][col];
+                rightPixel = pixels[row][width - 1 - col];
+                rightPixel.setColor(leftPixel.getColor());
+            }
+        } 
+    }
+
+    /** Mirror just part of a picture of a temple */
+    public void mirrorTemple()
+    {
+        int mirrorPoint = 276;
+        Pixel leftPixel = null;
+        Pixel rightPixel = null;
+        int count = 0;
+        Pixel[][] pixels = this.getPixels2D();
+
+        // loop through the rows
+        for (int row = 27; row < 97; row++)
+        {
+            // loop from 13 to just before the mirror point
+            for (int col = 13; col < mirrorPoint; col++)
+            {
+
+                leftPixel = pixels[row][col];      
+                rightPixel = pixels[row]                       
+                [mirrorPoint - col + mirrorPoint];
+                rightPixel.setColor(leftPixel.getColor());
+            }
+        }
+    }
+
+    /** copy from the passed fromPic to the
+     * specified startRow and startCol in the
+     * current picture
+     * @param fromPic the picture to copy from
+     * @param startRow the start row to copy to
+     * @param startCol the start col to copy to
+     */
+    public void copy(Picture fromPic, 
+    int startRow, int startCol)
+    {
+        Pixel fromPixel = null;
+        Pixel toPixel = null;
+        Pixel[][] toPixels = this.getPixels2D();
+        Pixel[][] fromPixels = fromPic.getPixels2D();
+        for (int fromRow = 0, toRow = startRow; 
+        fromRow < fromPixels.length &&
+        toRow < toPixels.length; 
+        fromRow++, toRow++)
+        {
+            for (int fromCol = 0, toCol = startCol; 
+            fromCol < fromPixels[0].length &&
+            toCol < toPixels[0].length;  
+            fromCol++, toCol++)
+            {
+                fromPixel = fromPixels[fromRow][fromCol];
+                toPixel = toPixels[toRow][toCol];
+                toPixel.setColor(fromPixel.getColor());
+            }
+        }   
+    }
+
+    /** Method to create a collage of several pictures */
+    public void createCollage()
+    {
+        Picture flower1 = new Picture("flower1.jpg");
+        Picture flower2 = new Picture("flower2.jpg");
+        this.copy(flower1,0,0);
+        this.copy(flower2,100,0);
+        this.copy(flower1,200,0);
+        Picture flowerNoBlue = new Picture(flower2);
+        flowerNoBlue.zeroBlue();
+        this.copy(flowerNoBlue,300,0);
+        this.copy(flower1,400,0);
+        this.copy(flower2,500,0);
+        this.mirrorVertical();
+        this.write("collage.jpg");
+    }
+
+    /** Method to show large changes in color 
+     * @param edgeDist the distance for finding edges
+     */
+    public void edgeDetection(int edgeDist)
+    {
+        Pixel leftPixel = null;
+        Pixel rightPixel = null;
+        Pixel[][] pixels = this.getPixels2D();
+        Color rightColor = null;
+        for (int row = 0; row < pixels.length; row++)
+        {
+            for (int col = 0; 
+            col < pixels[0].length-1; col++)
+            {
+                leftPixel = pixels[row][col];
+                rightPixel = pixels[row][col+1];
+                rightColor = rightPixel.getColor();
+                if (leftPixel.colorDistance(rightColor) > 
+                edgeDist)
+                    leftPixel.setColor(Color.BLACK);
+                else
+                    leftPixel.setColor(Color.WHITE);
+            }
+        }
+    }
+
+    /* Main method for testing - each class in Java can have a main 
+     * method 
+     */
+    public static void main(String[] args) 
+    {
+        Picture beach = new Picture("beach.jpg");
+        beach.explore();
+        beach.zeroBlue();
+        beach.explore();
+    }
+
 } // this } is the end of class Picture, put all new methods before this
--- PictureLabProject/pixLab/classes/PictureTester.java
+++ PictureLabProject/pixLab/classes/PictureTester.java
@@ -13,16 +13,124 @@ public class PictureTester
   /** Method to test zeroBlue */
   public static void testZeroBlue()
   {
-    Picture beach = new Picture("beach.jpg");
-    beach.explore();
-    beach.zeroBlue();
-    beach.explore();
+    Picture Walter = new Picture("beach.jpg");
+    Walter.explore();
+    Walter.zeroBlue();
+    Walter.explore();
   }
+  public static void testReddify()
+  {
+    Picture Walter = new Picture("beach.jpg");
+    Walter.explore();
+    Walter.redmodePic();
+    Walter.explore();
+  }
+  public static void testGreenify()
+  {
+    Picture Walter = new Picture("beach.jpg");
+    Walter.explore();
+    Walter.greenPic();
+    Walter.explore();
+  }
+  public static void testBlueify()
+  {
+    Picture Walter = new Picture("beach.jpg");
+    Walter.explore();
+    Walter.bluePic();
+    Walter.explore();
+  }
+  public static void testVoidify()
+  {
+    Picture Walter = new Picture("beach.jpg");
+    Walter.explore();
+    Walter.voidify();
+    Walter.explore();
+  }
+  public static void testAntiVoidify()
+  {
+    Picture Walter = new Picture("beach.jpg");
+    Walter.explore();
+    Walter.antivoidify();
+    Walter.explore();
+  }
+  public static void testNegative()
+  {
+    Picture Walter = new Picture("beach.jpg");
+    Walter.explore();
+    Walter.negativeImg();
+    Walter.explore();
+  }
+  public static void testZeroGreen()
+  {
+    Picture Walter = new Picture("beach.jpg");
+    Walter.explore();
+    Walter.zeroGreen();
+    Walter.explore();
+  }
+  public static void testFlipper()
+  {
+    Picture Walter = new Picture("beach.jpg");
+    Walter.explore();
+    Walter.flippyVoid();
+    Walter.explore();
+  }
+  public static void testBlackLines()
+  {
+    Picture Walter = new Picture("beach.jpg");
+    Walter.explore();
+    Walter.flippyBlack();
+    Walter.explore();
+  }
+  public static void testBlack()
+  {
+    Picture Walter = new Picture("beach.jpg");
+    Walter.explore();
+    Walter.floppyBlack();
+    Walter.explore();
+}
+  public static void testZeroRed()
   
+  {
+    Picture Walter = new Picture("beach.jpg");
+    Walter.explore();
+    Walter.zeroRed();
+    Walter.explore();
+  }
+  public static void testKeepOnlyBlue()
+  {
+    Picture Walter = new Picture("beach.jpg");
+    Walter.explore();
+    Walter.zeroRed();
+    Walter.zeroGreen();
+    Walter.explore();
+  }
+  public static void testKeepOnlyRed()
+  {
+    Picture Walter = new Picture("beach.jpg");
+    Walter.explore();
+    Walter.zeroBlue();
+    Walter.zeroGreen();
+    Walter.explore();
+  }
+  public static void testKeepOnlyGreen()
+  {
+    Picture Walter = new Picture("beach.jpg");
+    Walter.explore();
+    Walter.zeroRed();
+    Walter.zeroBlue();
+    Walter.explore();
+  }
+  public static void testGrayscale()
+  {
+    Picture Walter = new Picture("beach.jpg");
+    Walter.explore();
+    Walter.makeGrey();
+    Walter.explore();
+  }
   /** Method to test mirrorVertical */
   public static void testMirrorVertical()
   {
-    Picture caterpillar = new Picture("caterpillar.jpg");
+    Picture caterpillar = new Picture("beach.jpg");
     caterpillar.explore();
     caterpillar.mirrorVertical();
     caterpillar.explore();
@@ -31,7 +139,7 @@ public class PictureTester
   /** Method to test mirrorTemple */
   public static void testMirrorTemple()
   {
-    Picture temple = new Picture("temple.jpg");
+    Picture temple = new Picture("beach.jpg");
     temple.explore();
     temple.mirrorTemple();
     temple.explore();
@@ -40,7 +148,7 @@ public class PictureTester
   /** Method to test the collage method */
   public static void testCollage()
   {
-    Picture canvas = new Picture("640x480.jpg");
+    Picture canvas = new Picture("beach.jpg");
     canvas.createCollage();
     canvas.explore();
   }
@@ -48,7 +156,7 @@ public class PictureTester
   /** Method to test edgeDetection */
   public static void testEdgeDetection()
   {
-    Picture swan = new Picture("swan.jpg");
+    Picture swan = new Picture("beach.jpg");
     swan.edgeDetection(10);
     swan.explore();
   }
@@ -61,8 +169,10 @@ public class PictureTester
     // and comment out the ones you don't want
     // to run
     //testZeroBlue();
+    //testZeroRed();
+    //testZeroGreen();
     //testKeepOnlyBlue();
-    //testKeepOnlyRed();
+    testKeepOnlyRed();
     //testKeepOnlyGreen();
     //testNegate();
     //testGrayscale();
