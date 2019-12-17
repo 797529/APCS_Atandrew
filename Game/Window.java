333a071a71b314566c9a6fa57627c1c7663b57fd
797529 <54371358+797529@users.noreply.github.com>
2019-12-17 10:31:47 -0800

    added lwjgl

---
+++ Game/Window.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class Window here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Window
+{
+    public Window()
+    {
+        
+    }
+    public static void main(){
+        System.out.println(Color.RED + "Red?" + Color.RESET);
+    }
+}
